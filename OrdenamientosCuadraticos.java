3906ddb84a2ec6cf4660e8e91f339da5bd9113d6
Erick-XMB <erick.martinezkmn@ciencias.unam.mx>
2026-03-17 10:49:24 -0600

    iniciando

---
+++ OrdenamientosCuadraticos.java
@@ -0,0 +1,61 @@
+public class OrdenamientosCuadraticos {
+	
+	private static final int SELECTION_SORT = 1;
+	
+	private static final int BUBBLE_SORT = 2;
+	
+	private static final int INSERTION_SORT = 3;
+
+    private static <T> void intercambio(T[] arreglo, int i, int j) {
+        /*Aqui va tu codigo*/
+    }
+
+    private static <T extends Comparable<T>> int encontrarIndiceMenor(T[] arreglo, int i, int j){
+        /*Aqui va tu codigo*/
+    }  
+
+    public static <T extends Comparable<T>> void selectionSort(T[] arreglo, boolean imprimir) {     
+        /*Aqui va tu codigo*/
+    }
+
+    public static <T extends Comparable<T>> void bubbleSort(T[] arreglo,boolean imprimir) {
+        /*Aqui va tu codigo*/
+    }
+
+    public static <T extends Comparable<T>> void insertionSort(T[] arreglo, boolean imprimir) {
+        /*Aqui va tu codigo*/
+    }
+
+    public static <T extends Comparable<T>> void ordenar(int algoritmo, T[] arreglo, boolean imprimir) {
+        switch (algoritmo) {
+            case SELECTION_SORT:
+            
+                selectionSort(arreglo,imprimir);
+                break;
+                
+            case BUBBLE_SORT:
+                bubbleSort(arreglo,imprimir);
+                break;
+            
+            case INSERTION_SORT:
+                insertionSort(arreglo,imprimir);
+                break;
+                
+            default:
+                System.out.println("Opción de ordenamiento no válida.");
+        }
+    }
+
+    public static <T> String imprimeArreglo(T[] arreglo) {
+        String s = "{";
+        for (int i = 0; i < arreglo.length; i++) {
+            if (i == arreglo.length - 1) {
+                s = s + arreglo[i];
+            } else {
+                s = s + arreglo[i] + ",";
+            }
+        }
+        s = s + "}";
+        return s;
+    }
+}
